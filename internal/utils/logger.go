59164c944b723eb2bca53389e297e8e39188f265
Karim Assifar <82780260+Assifar-Karim@users.noreply.github.com>
2024-10-09 22:34:06 +0100

    Add trace logging functionality

--- internal/utils/logger.go
+++ internal/utils/logger.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"slices"
 	"sync"
 )
 
@@ -13,6 +14,7 @@ type Logger struct {
 	infoLogger  *log.Logger
 	warnLogger  *log.Logger
 	errorLogger *log.Logger
+	traceLogger *log.Logger
 }
 
 var loggerInstance *Logger
@@ -22,10 +24,18 @@ func GetLogger() *Logger {
 		lock.Lock()
 		defer lock.Unlock()
 		flags := log.Ldate | log.Ltime | log.Lmsgprefix
+
+		args := os.Args[1:]
+		var traceLogger *log.Logger = nil
+		if slices.Contains(args, "--trace") {
+			traceLogger = log.New(os.Stdout, "\033[32mTRACE: \033[0m", flags)
+		}
+
 		loggerInstance = &Logger{
 			infoLogger:  log.New(os.Stdout, "\033[35mINFO: \033[0m", flags),
 			warnLogger:  log.New(os.Stdout, "\033[33mWARN: \033[0m", flags),
 			errorLogger: log.New(os.Stderr, "\033[31mERROR: \033[0m", flags),
+			traceLogger: traceLogger,
 		}
 	}
 	return loggerInstance
@@ -41,6 +51,12 @@ func (l *Logger) Warn(format string, v ...interface{}) {
 
 func (l *Logger) Error(format string, v ...interface{}) {
 	l.errorLogger.Printf(format, v...)
+}
+
+func (l *Logger) Trace(format string, v ...interface{}) {
+	if l.traceLogger != nil {
+		l.traceLogger.Printf(format, v...)
+	}
 }
 
 func (l *Logger) PrintBanner() {
